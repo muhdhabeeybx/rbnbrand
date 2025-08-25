1fc42f08299337fdf0b02b64f0c557c9507b5c6e
muhdhabeeybx <122448219+muhdhabeeybx@users.noreply.github.com>
2025-08-25 08:34:43 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+// import fonts
+import "@fontsource/dm-serif-display/400.css";
+import "@fontsource/onest/400.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
