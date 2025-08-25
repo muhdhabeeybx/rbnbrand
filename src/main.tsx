21ada921c993f25c0e2135d72a0320f25d95caae
muhdhabeeybx <122448219+muhdhabeeybx@users.noreply.github.com>
2025-08-26 00:06:26 +0100

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-// import fonts
-import "@fontsource/dm-serif-display/400.css";
-import "@fontsource/onest/400.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
